06b86d3cba1e2382d92d67331eafa75cc88b2c39
Agustin <agualvila@gmail.com>
2021-11-24 22:09:42 -0300

    add projects router

--- routes/projects.js
+++ routes/projects.js
@@ -1,3 +1,16 @@
-const router = require('express').Router();
+const express = require('express');
+const projectsRouter = express.Router();
+const projectsController = require('../controllers/projects');
+const pagination = require('../middlewares/pagination');
 
-module.exports = router;
+projectsRouter.get('/', pagination.validate, projectsController.getAll);
+
+projectsRouter.get('/:id', projectsController.getById);
+
+projectsRouter.post('/', projectsController.create);
+
+projectsRouter.post('/:id', projectsController.update);
+
+projectsRouter.delete('/:id', projectsController.remove);
+
+module.exports = projectsRouter;
