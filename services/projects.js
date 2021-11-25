7c332a705f2043571cb7fdfbffa1728298f08829
Agustin <agualvila@gmail.com>
2021-11-25 16:13:12 -0300

    map functions refactor

--- services/projects.js
+++ services/projects.js
@@ -1,8 +1,57 @@
 const projectsRepository = require('../repositories/projects');
 const usersRepository = require('../repositories/users');
 const projectAssigneesRepository = require('../repositories/projectAssignees');
-
 const paginateRequest = require('../services/paginateRequest');
+
+const verifyAsignees = async (assignees) => {
+  if (assignees.length > 0) {
+    // Promise para manejo asíncrono de forEach
+    const checkUsers = new Promise((resolve, reject) => {
+      assignees.forEach(async (value, index, array) => {
+        const userExist = await usersRepository.getById(value);
+        if (!userExist) {
+          reject({ message: `The userId: ${value} doesen't exists` });
+        }
+        if (index === array.length - 1) resolve();
+      });
+    });
+
+    try {
+      await checkUsers.then(() => {
+        console.log('ok!');
+      });
+    } catch (e) {
+      console.log(e);
+      const error = new Error(e.message);
+      error.status = 400;
+      throw error;
+    }
+  }
+};
+
+const mapProjectResponse = (project) => {
+  const assignees = project.assignees;
+
+  const assigneesMap = assignees.map((item) => {
+    const newItem = {
+      userId: item.id,
+      firstName: item.firstName,
+      lastName: item.lastName
+    };
+    return newItem;
+  });
+
+  const projectResponse = {
+    id: project.id,
+    name: project.name,
+    projectStatus: project.projectStatus,
+    projectManager: project.projectManager,
+    assignees: assigneesMap,
+    createdAt: project.createdAt,
+    updatedAt: project.updatedAt
+  };
+  return projectResponse;
+};
 
 const getAll = async (req) => {
   const limit = Number(req.query.limit);
@@ -21,27 +70,7 @@ const getAll = async (req) => {
   let projectsResponse = [];
 
   projects.forEach((proj) => {
-    const assignees = proj.assignees;
-
-    const assigneesMap = assignees.map((item) => {
-      const newItem = {
-        userId: item.id,
-        firstName: item.firstName,
-        lastName: item.lastName
-      };
-      return newItem;
-    });
-
-    //
-    const project = {
-      id: proj.id,
-      name: proj.name,
-      projectStatus: proj.projectStatus,
-      projectManager: proj.projectManager,
-      assignees: assigneesMap,
-      createdAt: proj.createdAt,
-      updatedAt: proj.updatedAt
-    };
+    const project = mapProjectResponse(proj);
 
     projectsResponse.push(project);
   });
@@ -76,34 +105,13 @@ const getById = async (id) => {
     throw error;
   }
 
-  const assignees = project.assignees;
+  const projectMapped = mapProjectResponse(project);
 
-  const assigneesMap = assignees.map((item) => {
-    const newItem = {
-      userId: item.id,
-      firstName: item.firstName,
-      lastName: item.lastName
-    };
-    return newItem;
-  });
-
-  //
-  const projectResponse = {
-    id: project.id,
-    name: project.name,
-    projectStatus: project.projectStatus,
-    projectManager: project.projectManager,
-    assignees: assigneesMap,
-    createdAt: project.createdAt,
-    updatedAt: project.updatedAt
-  };
-
-  return projectResponse;
+  return projectMapped;
 };
 
 const create = async (projectData) => {
-  const { name, description, projectManagerId, projectStatusId, assignees } =
-    projectData;
+  const { name, assignees } = projectData;
 
   //verificar que no exista el mismo nombre
   const projectExist = await projectsRepository.getByName(name);
@@ -114,29 +122,7 @@ const create = async (projectData) => {
   }
 
   // verificar que existan ids de assignees en users
-  if (assignees.length > 0) {
-    // Promise para manejo asíncrono de forEach
-    const checkUsers = new Promise((resolve, reject) => {
-      assignees.forEach(async (value, index, array) => {
-        const userExist = await usersRepository.getById(value);
-        if (!userExist) {
-          reject({ message: `The userId: ${value} doesen't exists` });
-        }
-        if (index === array.length - 1) resolve();
-      });
-    });
-
-    try {
-      await checkUsers.then(() => {
-        console.log('ok!');
-      });
-    } catch (e) {
-      console.log(e);
-      const error = new Error(e.message);
-      error.status = 400;
-      throw error;
-    }
-  }
+  verifyAsignees(assignees);
 
   // crear nuevo project
   const project = await projectsRepository.create(projectData);
@@ -170,21 +156,26 @@ const create = async (projectData) => {
   }
 
   const newProject = await projectsRepository.getById(project.id);
-  return newProject;
+
+  const newProjectMapped = mapProjectResponse(newProject);
+
+  return newProjectMapped;
 };
 
-const update = async (
-  id,
-  { name, description, projectManagerId, assignees, projectStatusId }
-) => {
-  //traer projectAssigneesNew a partir de array assignees
+const update = async (id, projectData) => {
+  const { name, description, projectManagerId, assignees, projectStatusId } =
+    projectData;
 
+  // verificar que exita el proyecto
   const project = await projectsRepository.getById(id);
   if (!project) {
     const error = new Error('Project not found');
     error.status = 404;
     throw error;
   }
+
+  //traer projectAssigneesNew a partir de array assignees
+
   await projectsRepository.update(id, {
     name,
     description,
