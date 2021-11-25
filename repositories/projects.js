9843954fdaba56b2ca3479430bd352b26b0f72f0
Agustin <agualvila@gmail.com>
2021-11-24 22:08:00 -0300

    add project repositories

---
+++ repositories/projects.js
@@ -0,0 +1,48 @@
+const db = require('../models');
+
+const getAll = async () => {
+  const response = await db.Project.findAll();
+  return response;
+};
+
+const getById = async (id) => {
+  const response = await db.Project.findByPk(id);
+  return response;
+};
+
+const getCount = async () => {
+  const response = await db.Project.count();
+  return response;
+};
+
+const create = async (data) => {
+  const response = await db.Project.create(data);
+  return response;
+};
+
+const update = async (id, data) => {
+  const response = await db.Project.update(data, {
+    where: {
+      id
+    }
+  });
+  return response;
+};
+
+const remove = async (id) => {
+  const response = await db.Project.destroy({
+    where: {
+      id
+    }
+  });
+  return response;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  getCount,
+  create,
+  update,
+  remove
+};
