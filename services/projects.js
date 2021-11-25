799cbb2a8ab399d35be7f6f76e734f9a2bcd5849
Agustin <agualvila@gmail.com>
2021-11-25 13:48:15 -0300

    create project with assignees feature workInProgr

--- services/projects.js
+++ services/projects.js
@@ -1,4 +1,5 @@
 const projectsRepository = require('../repositories/projects');
+const usersRepository = require('../repositories/users');
 const paginateRequest = require('../services/paginateRequest');
 
 const getAll = async (req) => {
@@ -38,23 +39,67 @@ const getAll = async (req) => {
 };
 
 const getById = async (id) => {
-  const projects = await projectsRepository.getById(id);
-  if (!projects) {
+  const project = await projectsRepository.getById(id);
+  if (!project) {
     const error = new Error('Project not found');
     error.status = 404;
     throw error;
   }
-  return projects;
+  return project;
 };
 
 const create = async (projectData) => {
-  return await projectsRepository.create(projectData);
+  const { name, description, projectManagerId, projectStatusId, assignees } =
+    projectData;
+
+  //verificar que no exista el mismo nombre
+  const projectExist = await projectsRepository.getByName(name);
+  if (projectExist) {
+    const error = new Error('Project name already exists');
+    error.status = 400;
+    throw error;
+  }
+
+  // verificar que existan ids de assignees en users
+  if (assignees.length > 0) {
+    // Promise para manejo asíncrono de forEach
+    const checkUsers = new Promise((resolve, reject) => {
+      assignees.forEach(async (value, index, array) => {
+        const userExist = await usersRepository.getById(value);
+        if (!userExist) {
+          reject({ message: `The userId: ${value} doesen't exists` });
+        }
+        if (index === array.length - 1) resolve();
+      });
+    });
+
+    try {
+      await checkUsers.then(() => {
+        console.log('ok!');
+      });
+    } catch (e) {
+      console.log(e);
+      const error = new Error(e.message);
+      error.status = 400;
+      throw error;
+    }
+  }
+
+  // crear nuevo project
+  const project = await projectsRepository.create(projectData);
+
+  // agregar assignees en projectAssignees mediante ids assignees y project.id
+
+  const newProject = await projectsRepository.getById(project.id);
+  return newProject;
 };
 
 const update = async (
   id,
   { name, description, projectManagerId, assignees, projectStatusId }
 ) => {
+  //traer projectAssigneesNew a partir de array assignees
+
   const project = await projectsRepository.getById(id);
   if (!project) {
     const error = new Error('Project not found');
